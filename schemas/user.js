4836562433e300f5e0fdee78562a9abf9c83e876
Brigen31 <brigen31bercaj@gmail.com>
2025-11-17 10:39:48 +0100

    fix: user id dto

--- schemas/user.js
+++ schemas/user.js
@@ -1,7 +1,6 @@
 const Joi = require("joi");
 
 module.exports = Joi.object({
-    id: Joi.number().integer().required(),
     username: Joi.string().trim().min(1).max(30).required(),
     password: Joi.string().min(1).max(100).required(),
     isAdmin: Joi.boolean().required(),
